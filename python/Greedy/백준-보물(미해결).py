107e535e8ce0e444347be1335bb622bfafe16119
younhwan97 <younhwan0903@gmail.com>
2022-06-02 19:12:40 +0900

    22.06.02

--- python/Greedy/백준-보물(미해결).py
+++ python/Greedy/백준-보물(미해결).py
@@ -2,20 +2,11 @@ n = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-def s(arr1, arr2):
-    result = 0
-    for i in range(0, n):
-        result += (arr1[i] * arr2[i])
-    return result
+answer = 0
 
-## 배열 a를 오름차순 정렬
-a.sort()
+for i in range(0, n):
+    a.remove(min(a))
+    b.remove(max(b))
+    answer += (min(a) * max(b))
 
-temp = list()
-
-# for i in range(0, n):
-    
-            
-print(s(a, b))
-
-
+print(answer)
