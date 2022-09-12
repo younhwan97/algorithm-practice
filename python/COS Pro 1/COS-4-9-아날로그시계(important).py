0dc849665f6c562a375793a31c2b17942b3cb2e6
younhwan97 <younhwan0903@gmail.com>
2022-09-12 17:31:10 +0900

    22.09.12

--- python/COS Pro 1/COS-4-9-아날로그시계(important).py
+++ python/COS Pro 1/COS-4-9-아날로그시계(important).py
@@ -2,12 +2,18 @@
 # import math
 
 def solution(hour, minute):
-    answer = ''
+    # 분침
+    m = (360 / 60) * minute
+
+    # 시침
+    h = (360 / 12) * hour + (360/12/60) * minute
+
+    answer = round(abs(h - m), 1) if abs(h - m) < 180 else round(360 - abs(h-m), 1)
     return answer
 
 # 아래는 테스트케이스 출력을 해보기 위한 코드입니다.
-hour = 3
-minute = 0
+hour = 2
+minute = 50
 ret = solution(hour, minute)
 
 # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
