60a335affa5b2164eae25862bad96a425b05a3ba
younhwan97 <younhwan0903@gmail.com>
2022-09-17 12:03:06 +0900

    22.09.17

--- python/COS Pro 1/COS-2-4-서로다른세개숫자의합이K의배수인것(다시).py
+++ python/COS Pro 1/COS-2-4-서로다른세개숫자의합이K의배수인것(다시).py
@@ -1,6 +1,7 @@
 #다음과 같이 import를 사용할 수 있습니다.
 #import math
 
+## 반복을 이용한 방법
 def solution(arr, K):
     #여기에 코드를 작성해주세요.
     answer = 0
@@ -15,14 +16,37 @@ def solution(arr, K):
 
     return answer
 
-## 다른 방법 없을까?
-### 리스트의 크기가 커지면 무조건 시간초과가 발생하는데..
-### dp?
+## 재귀를 이용한 방법
+result = []
+def pick_number(cnt, K, number_cnt, used_number):
+    if cnt == 3:
+        temp = list()
+        for i in range(len(used_number)):
+            if used_number[i] != 0:
+                temp.append(i)
 
+        if (temp[0] + temp[1] + temp[2]) % K == 0 and (temp[0], temp[1], temp[2]) not in result:
+            result.append((temp[0], temp[1], temp[2]))
+        return
+
+    for i in range(len(number_cnt)):
+        if number_cnt[i] > used_number[i]:
+            used_number[i] += 1
+            pick_number(cnt + 1, K, number_cnt, used_number)
+            used_number[i] -= 1
+    
 #아래는 테스트케이스 출력을 해보기 위한 코드입니다.
 arr = [1, 2, 3, 4, 5]
-K = 3
-ret = solution(arr, K)
 
+number_cnt = [0] * (max(arr) + 1)
+used_number = [0] * (max(arr) + 1)
+for i in range(len(arr)): number_cnt[arr[i]] += 1
+
+K = 3
+
+pick_number(0, K, number_cnt, used_number)
+print(len(result))
+
+# ret = solution(arr, K)
 #[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-print("solution 함수의 반환 값은 ", ret, " 입니다.")
+# print("solution 함수의 반환 값은 ", ret, " 입니다.")
